aa2a6654f0c978dbaf3507b544d8da4a7b7a4323
Sann11-y <sann111126@gmail.com>
2026-06-15 14:55:24 +0700

    qkdq

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Nemu.id</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
-    <link rel="stylesheet" href="assets/css/custom.css"/>
+    <link rel="stylesheet" href="/Nemu.id/assets/css/custom.css"/>
 </head>
 <body>
 
