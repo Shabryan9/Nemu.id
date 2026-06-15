2248d5b0c32ff52065b3ba10959bdf24a5eab2d6
Onjie <honziekaori@gmail.com>
2026-06-15 14:32:37 +0700

    nambah header

--- includes/header.php
+++ includes/header.php
@@ -1,15 +1,15 @@
-<?php session_start(); ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Nemu.id</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
+  <link rel="stylesheet" href="assets/css/style.css"/>
 </head>
 <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    <nav class="navbar navbar-expand-lg navbar-custom px-3">
+ 
+<nav class="navbar navbar-expand-lg navbar-custom px-3">
   <!-- Brand -->
   <a class="navbar-brand" href="#">Nemu.id</a>
  
@@ -32,5 +32,7 @@
     <button class="btn btn-masuk">Masuk</button>
   </div>
 </nav>
+ 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
