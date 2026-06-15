fb80cfd96ed487b90d194cac32bac6c7f47e9290
Onjie <honziekaori@gmail.com>
2026-06-15 14:24:18 +0700

    nambahin header

---
+++ includes/header.php
@@ -0,0 +1,36 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <nav class="navbar navbar-expand-lg navbar-custom px-3">
+  <!-- Brand -->
+  <a class="navbar-brand" href="#">Nemu.id</a>
+ 
+  <!-- Toggler untuk mobile -->
+  <button class="navbar-toggler border-0" type="button"
+    data-bs-toggle="collapse" data-bs-target="#navbarContent"
+    aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon" style="filter: invert(1);"></span>
+  </button>
+ 
+  <!-- Nav Links -->
+  <div class="collapse navbar-collapse" id="navbarContent">
+    <ul class="navbar-nav me-auto mb-0">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Beranda</a>
+      </li>
+    </ul>
+ 
+    <!-- Tombol Masuk -->
+    <button class="btn btn-masuk">Masuk</button>
+  </div>
+</nav>
+</body>
+</html>
